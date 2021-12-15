6158b0c4344904af6889b271abbadd962d1f7008
vantral <anton.buzanov.00@gmail.com>
2021-12-15 16:01:17 +0300

    footer again

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -4,11 +4,17 @@ var cite = document.getElementById("modalcitation");
 // Get the button that opens the modal
 var btncite = document.getElementById("citationbtn");
 
+var btncite1 = document.getElementById("citationbtn1");
+
 // Get the <span> element that closes the modal
 var citespan = document.getElementById("citeclose");
 
 // When the user clicks the button, open the modal 
 btncite.onclick = function() {
+    cite.style.display = "block";
+}
+
+btncite1.onclick = function() {
     cite.style.display = "block";
 }
 
